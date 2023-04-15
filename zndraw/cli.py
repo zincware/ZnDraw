5ed0a06384936c88645887ac3486986febf81b90
PythonFZ <fabian.zills@web.de>
2023-04-15 22:31:37 +0200

    insert path

--- zndraw/cli.py
+++ zndraw/cli.py
@@ -1,40 +1,9 @@
 import typer
 from zndraw import app, globals
+import pathlib
+import sys
 
 cli = typer.Typer()
-
-
-# @app.command()
-# def main(
-#     file: str,
-#     select_canvas: bool = typer.Option(True),
-#     select_atoms: bool = typer.Option(False),
-#     bonds: bool = typer.Option(False),
-# ) -> None:
-#     """ZnDraw: Visualize Molecules
-
-#     With 'zndraw molecule.xyz' you can visualize the molecule in your browser.
-
-#     Attributes
-#     ----------
-#     file : str
-#         Path to the xyz file.
-#     select_canvas : bool
-#         Add a canvas that helps to select points in space.
-#     select_atoms : bool
-#         Allow selection of atoms.
-#     """
-#     graph = io.read_file(file)
-
-#     app = viewer.DashApp(
-#         graph=graph, select_canvas=select_canvas, select_atoms=select_atoms
-#     )
-#     app.update_layout()
-#     app.add_canvas_slider()
-#     app.plot_atoms()
-#     if bonds:
-#         app.plot_bonds()
-#     app.run_dash_server_click()
 
 
 @cli.command()
@@ -64,6 +33,8 @@ def main(
 
     CLI for the ZnDraw visualizer.
     """
+    sys.path.insert(1, pathlib.Path.cwd().as_posix())
+
     globals.config.file = file
     globals.config.animate = animate
     globals.config.sphere_size = sphere_size
