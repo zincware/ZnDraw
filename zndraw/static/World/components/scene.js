6ba4f96baaedebb52329c8a757f6887ae6b31d29
PythonFZ <fabian.zills@web.de>
2023-05-15 13:02:36 +0200

    add html back

--- zndraw/static/World/components/scene.js
+++ zndraw/static/World/components/scene.js
@@ -3,7 +3,7 @@ import { Color, Scene } from 'three';
 function createScene() {
   const scene = new Scene();
 
-  scene.background = new Color('skyblue');
+  scene.background = new Color('white');
 
   return scene;
 }
