913b001d91f6285ac22ead011d844f2b5c94301d
PythonFZ <fabian.zills@web.de>
2023-05-22 13:13:12 +0200

    fix bonds

--- zndraw/static/World/components/particles.js
+++ zndraw/static/World/components/particles.js
@@ -118,40 +118,36 @@ export function createParticleGroup(config) {
     const particles = data["particles"];
     const bonds = data["bonds"];
 
-    // iterate new only, remove all that are not in new
 
-    // can we compute the differences and intersections in a single loop?
-
-    
-    // iterate difference between existing and new
-    // and remove all that are not in new
-
-    // iterate intersection between existing and new
-    // and update
-
-    // iterate difference between new and existing
-    // and create
-
-    // for particle in existing + new
-    // new.get() -> update / create
-    // else remove
-
-    // for bond in existing + new
-    // new.get() -> update / create
-    // else remove
-
+    // create particle arrays
     let existing_particles = particles.filter(x => particleGroup.getObjectByName(x.id));
     let new_particles = particles.filter(x => !particleGroup.getObjectByName(x.id));
     let deleted_particles = particleGroup.children.filter(x => !particles.find(y => y.id === x.name));
 
-    console.log("Having existing particles: " + existing_particles.length + " and adding " + new_particles.length + " and removing " + deleted_particles.length);
+    // create bond arrays
+    let all_bonds = [];
+    particleGroup.children.forEach((particleSubGroup) => {
+      particleSubGroup.children.forEach((item, idx) => {
+        if (idx === 0) {
+          return;
+        }
+        all_bonds.push(item);
+      });
+    });
+
+    let existing_bonds = all_bonds.filter(x => bonds.find(y => y[0] + "-" + y[1] === x.name) || bonds.find(y => y[1] + "-" + y[0] === x.name));
+    let new_bonds = bonds.filter(x => !all_bonds.find(y => y.name === x[0] + "-" + x[1]) && !all_bonds.find(y => y.name === x[1] + "-" + x[0]));
+    let deleted_bonds = all_bonds.filter(x => !bonds.find(y => y[0] + "-" + y[1] === x.name) && !bonds.find(y => y[1] + "-" + y[0] === x.name));
+
+    // console.log("Having existing particles: " + existing_particles.length + " and adding " + new_particles.length + " and removing " + deleted_particles.length);
+    // console.log("Having existing bonds: " + existing_bonds.length + " and adding " + new_bonds.length + " and removing " + deleted_bonds.length);
 
     // update existing particles
     existing_particles.forEach((particle) => {
       const particleSubGroup = particleGroup.getObjectByName(particle.id);
 
       particleSubGroup.position.set(particle.x, particle.y, particle.z);
-      
+
       let material = speciesMaterial(
         config.config.material,
         particle.color,
@@ -185,112 +181,95 @@ export function createParticleGroup(config) {
         ),
       );
       const particleSubGroup = new THREE.Group();
-        particleSubGroup.add(particle_mesh);
-        particleSubGroup.name = particle.id;
-        particleSubGroup.position.set(particle.x, particle.y, particle.z);
+      particleSubGroup.add(particle_mesh);
+      particleSubGroup.name = particle.id;
+      particleSubGroup.position.set(particle.x, particle.y, particle.z);
 
-        // CLICK EVENT
-        particleSubGroup.click = () => {
-          if (config.selected.includes(particle.id)) {
-            if (!config.pressed_keys.Shift) {
-              config.selected = [];
-            } else {
-              config.selected = config.selected.filter((e) => e !== particle.id);
-            }
+      // CLICK EVENT
+      particleSubGroup.click = () => {
+        if (config.selected.includes(particle.id)) {
+          if (!config.pressed_keys.Shift) {
+            config.selected = [];
           } else {
-            if (!config.pressed_keys.Shift) {
-              config.selected = [particle.id];
-            } else {
-              config.selected.push(particle.id);
-            }
+            config.selected = config.selected.filter((e) => e !== particle.id);
           }
-        };
+        } else {
+          if (!config.pressed_keys.Shift) {
+            config.selected = [particle.id];
+          } else {
+            config.selected.push(particle.id);
+          }
+        }
+      };
 
-        particleGroup.add(particleSubGroup);
+      particleGroup.add(particleSubGroup);
     });
 
     // remove deleted particles
     deleted_particles.forEach((particle) => {
-      particleGroup.remove(particle);
+      particle.removeFromParent();
+      config.selected = config.selected.filter((e) => e !== particle.name);
     });
 
-    // remove bonds that are not in data
-    particleGroup.children.forEach((particle) => {
-      particle.children.forEach((bond, idx) => {
-        if (idx === 0) {
-          // entry 0 is the particle itself
-          return;
-        }
-        const bond_a = bonds.filter(
-          (item) => item[0] + "-" + item[1] === bond.name,
-        );
-        const bond_b = bonds.filter(
-          (item) => item[1] + "-" + item[0] === bond.name,
-        );
-
-        if (bond_a.length === 0 && bond_b.length === 0) {
-          // console.log("Removing bond " + bond.name + " in " + bonds[0]);
-          particle.remove(bond);
-        }
-      });
-    });
-
-    // now update bonds
     if (config.config.bond_size > 0) {
-      bonds.forEach((item) => {
-        const particle1SubGroup = particleGroup.getObjectByName(item[0]);
-        const particle2SubGroup = particleGroup.getObjectByName(item[1]);
+      // remove bonds that are not in data
+      deleted_bonds.forEach((bond) => {
+        bond.removeFromParent();
+      });
+      new_bonds.forEach((bond) => {
+        const particle1SubGroup = particleGroup.getObjectByName(bond[0]);
+        const particle2SubGroup = particleGroup.getObjectByName(bond[1]);
         const particle1 = particle1SubGroup.children[0];
         const particle2 = particle2SubGroup.children[0];
 
         const node1 = new THREE.Vector3();
         const node2 = new THREE.Vector3();
 
-        particle1.getWorldPosition(node1);
-        particle2.getWorldPosition(node2);
+        // const direction = new THREE.Vector3();
+        // direction.subVectors(node1, node2);
 
-        // existing bonds
-        let bond_1 = particleGroup.getObjectByName(item[0] + "-" + item[1]);
-        let bond_2 = particleGroup.getObjectByName(item[1] + "-" + item[0]);
+        const bond_1 = halfCylinderMesh(
+          node1,
+          node2,
+          particle1.material,
+          config.config,
+        );
+        const bond_2 = halfCylinderMesh(
+          node2,
+          node1,
+          particle2.material,
+          config.config,
+        );
 
-        const direction = new THREE.Vector3();
-        direction.subVectors(node1, node2);
-
-        if (bond_1 && bond_2) {
-          // update bond orientation
+        bond_1.tick = () => {
+          particle1.getWorldPosition(node1);
+          particle2.getWorldPosition(node2);
           updateBondOrientation(bond_1, node1, node2);
-          updateBondOrientation(bond_2, node2, node1);
           bond_1.material = particle1.material;
-          bond_2.material = particle2.material;
-        } else {
-          // temporary config
-          // console.log("Creating new bond");
-
-          bond_1 = halfCylinderMesh(
-            node1,
-            node2,
-            particle1.material,
-            config.config,
-          );
-          bond_2 = halfCylinderMesh(
-            node2,
-            node1,
-            particle2.material,
-            config.config,
-          );
-
-          // the atom to look at
-          bond_1.name = item[0] + "-" + item[1];
-          bond_2.name = item[1] + "-" + item[0];
-
-          particle1SubGroup.add(bond_1);
-          particle2SubGroup.add(bond_2);
-
-          // update bond orientation
-          updateBondOrientation(bond_1, node1, node2);
+        };
+        bond_2.tick = () => {
+          particle1.getWorldPosition(node1);
+          particle2.getWorldPosition(node2);
           updateBondOrientation(bond_2, node2, node1);
-        }
+          bond_2.material = particle2.material;
+        };
+
+        // the atom to look at
+        bond_1.name = bond[0] + "-" + bond[1];
+        bond_2.name = bond[1] + "-" + bond[0];
+
+        particle1SubGroup.add(bond_1);
+        particle2SubGroup.add(bond_2);
+
+        bond_1.tick();
+        bond_2.tick();
       });
+
+      // update existing bonds
+      existing_bonds.forEach((bond) => {
+        bond.tick();
+      });
+
     }
   };
 
