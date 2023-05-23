4177a6eef16309fdfbede0c111e9230cd1397f5a
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2023-05-23 11:35:31 +0200

    show indicies (#93)
    * show indicies
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove log
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- zndraw/static/World/systems/Resizer.js
+++ zndraw/static/World/systems/Resizer.js
@@ -7,13 +7,14 @@ const setSize = (container, camera, renderer) => {
 };
 
 class Resizer {
-  constructor(container, camera, renderer) {
+  constructor(container, camera, renderer, renderer2d) {
     // set initial size on load
     setSize(container, camera, renderer);
 
     window.addEventListener("resize", () => {
       // set the size again if a resize occurs
       setSize(container, camera, renderer);
+      renderer2d.setSize(window.innerWidth, window.innerHeight);
       // perform any custom actions
       this.onResize();
     });
