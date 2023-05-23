7a99a458d1921852075988e283d593f8210ae911
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2023-05-23 11:35:31 +0200

    show indicies (#93)
    * show indicies
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove log
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- zndraw/static/World/systems/Loop.js
+++ zndraw/static/World/systems/Loop.js
@@ -4,10 +4,11 @@ const clock = new Clock();
 const constraint_clock = new Clock();
 
 class Loop {
-  constructor(camera, scene, renderer, stream, config) {
+  constructor(camera, scene, renderer, renderer2d, stream, config) {
     this.camera = camera;
     this.scene = scene;
     this.renderer = renderer;
+    this.renderer2d = renderer2d;
     this.stream = stream;
     this.updatables = [];
     this.constraint_updatables = [];
@@ -31,6 +32,7 @@ class Loop {
   tick() {
     // only call the getDelta function once per frame!
     const delta = clock.getElapsedTime();
+    this.renderer2d.render(this.scene, this.camera);
 
     for (const object of this.updatables) {
       object.tick(delta);
--- zndraw/static/World/systems/renderer.js
+++ zndraw/static/World/systems/renderer.js
@@ -1,4 +1,5 @@
 import { WebGLRenderer } from "three";
+import { CSS2DRenderer } from "three/examples/jsm/renderers/CSS2DRenderer.js";
 
 function createRenderer() {
   const renderer = new WebGLRenderer({ antialias: true });
@@ -6,4 +7,17 @@ function createRenderer() {
   return renderer;
 }
 
-export { createRenderer };
+function create2DRenderer() {
+  const labelRenderer = new CSS2DRenderer();
+  labelRenderer.setSize(window.innerWidth, window.innerHeight);
+  labelRenderer.domElement.style.position = "absolute";
+  labelRenderer.domElement.style.top = "0px";
+  labelRenderer.domElement.style.pointerEvents = "none";
+  document
+    .getElementById("scene-container")
+    .appendChild(labelRenderer.domElement);
+
+  return labelRenderer;
+}
+
+export { createRenderer, create2DRenderer };
