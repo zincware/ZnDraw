0dcebd533be1ec6e8710b2ac7ad944af9fdce251
Rokas Elijošius <re344@cam.ac.uk>
2023-05-23 15:04:14 +0100

    Interactive controls for setting the bond thresholds
    Also should allow to add modifications to bond order manually
    
    use dict for storing bond modifications
    
    reformat

--- zndraw/tools/data.py
+++ zndraw/tools/data.py
@@ -5,9 +5,10 @@ import ase
 import networkx as nx
 import numpy as np
 from ase.data.colors import jmol_colors
-from ase.neighborlist import build_neighbor_list
-
+from ase.neighborlist import build_neighbor_list, natural_cutoffs
+from pydantic import BaseModel, Field
 from zndraw import shared
+
 
 def _rgb2hex(data):
     r, g, b = np.array(data * 255, dtype=int)
@@ -15,11 +16,16 @@ def _rgb2hex(data):
 
 
 class ASEComputeBonds(BaseModel):
-    double_bonds: float = Field(1, le=10, ge=1)
-    triple_bonds: float = Field(5, le=10, ge=1)
+    single_bond_multiplier: float = Field(1.0, le=10, ge=0)
+    double_bond_multiplier: float = Field(0.85, le=10, ge=0)
+    triple_bond_multiplier: float = Field(0.7, le=10, ge=0)
 
     def get_frame(self, step: int):
         atoms = shared.config.get_atoms(step=int(step))
+        atoms.info["graph_representation"] = self.build_graph(
+            atoms
+        )  ## TODO: Can probably add a try except here to avoid rebuilding the graph every time
+        self.update_graph_using_modifications(atoms)
         return {
             "particles": [
                 {
@@ -36,12 +42,40 @@ class ASEComputeBonds(BaseModel):
             "bonds": self.get_bonds(atoms),
         }
 
+    def build_graph(self, atoms: ase.Atoms):
+        cutoffs = [
+            self.single_bond_multiplier,
+            self.double_bond_multiplier,
+            self.triple_bond_multiplier,
+        ]
+        connectivity_matrix = np.zeros((len(atoms), len(atoms)), dtype=int)
+        for cutoff in cutoffs:
+            nl = build_neighbor_list(
+                atoms, cutoffs=natural_cutoffs(atoms, cutoff), self_interaction=False
+            )
+            connectivity_matrix += nl.get_connectivity_matrix(sparse=False)
+        G = nx.from_numpy_array(connectivity_matrix)
+        return G
+
+    def update_graph_using_modifications(self, atoms: ase.Atoms):
+        modifications = atoms.info.get("modifications", {})
+        graph = atoms.info["graph_representation"]
+        for key in modifications:
+            atom_1, atom_2 = key
+            weight = modifications[key]
+            graph.add_edge(atom_1, atom_2, weight=weight)
+
     def get_bonds(self, atoms: ase.Atoms):
-        atoms.pbc = False
-        nl = build_neighbor_list(atoms, self_interaction=False)
-        cm = nl.get_connectivity_matrix(sparse=False)
-        G = nx.from_numpy_array(cm)
-        return list(G.edges)
-    
-    def update_bond_order(self, particles: list[int], order: int):
-        pass
+        graph = atoms.info["graph_representation"]
+        bonds = []
+        for edge in graph.edges:
+            bonds.append((edge[0], edge[1], graph.edges[edge]["weight"]))
+        return bonds
+
+    def update_bond_order(self, atoms: ase.Atoms, particles: list[int], order: int):
+        if len(particles) != 2:
+            raise ValueError("Exactly two particles must be selected")
+        modifications = atoms.info.get("modifications", {})
+        sorted_particles = tuple(sorted(particles))
+        modifications[sorted_particles] = order
+        atoms.info["modifications"] = modifications
