30be53911e0a03bef18a3b18cf70f76adf08451e
Fabian <46721498+PythonFZ@users.noreply.github.com>
2023-09-01 15:36:05 +0200

    add orthographic camera

--- zndraw/static/World/components/camera.js
+++ zndraw/static/World/components/camera.js
@@ -1,11 +1,17 @@
 import * as THREE from "three";
 
 function createCamera() {
-  const camera = new THREE.PerspectiveCamera(
-    35, // fov = Field Of View
-    1, // aspect ratio (dummy value)
-    0.1, // near clipping plane
-    1000, // far clipping plane
+  // const camera = new THREE.PerspectiveCamera(
+  //   35, // fov = Field Of View
+  //   1, // aspect ratio (dummy value)
+  //   0.1, // near clipping plane
+  //   1000, // far clipping plane
+  // );
+  let width = window.innerWidth;
+	let height = window.innerHeight;
+
+  const camera = new THREE.OrthographicCamera(
+    width / - 2, width / 2, height / 2, height / - 2
   );
 
   // move the camera back so we can view the scene
