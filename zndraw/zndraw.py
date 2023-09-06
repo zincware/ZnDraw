d0e92370977c260416c13f47423d58647417917c
Fabian <93372652+Ruttor@users.noreply.github.com>
2023-09-06 17:43:57 +0200

    getitem slicing with negative numbers and slices (#187)
    * getitem slicing with negative numbers and slices
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- zndraw/zndraw.py
+++ zndraw/zndraw.py
@@ -97,10 +97,12 @@ class ZnDraw(collections.abc.MutableSequence):
     def __getitem__(self, index) -> t.Union[ase.Atoms, list[ase.Atoms]]:
         get_item_event = threading.Event()
 
-        if not isinstance(index, int) and not isinstance(index, list):
-            raise TypeError("Index must be an integer or list of integers")
+        length = len(self)
+        if isinstance(index, slice):
+            index = range(*index.indices(length))
 
         index = [index] if isinstance(index, int) else index
+        index = [i if i >= 0 else length + i for i in index]
 
         self.socket.emit("atoms:download", index)
 
