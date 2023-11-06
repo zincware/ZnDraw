1286aa77e2077754008aac0398da22179c5f6056
PythonFZ <fabian.zills@web.de>
2023-11-06 16:34:33 +0100

    move to events and cleanup code

--- zndraw/static/World/systems/events.js
+++ zndraw/static/World/systems/events.js
@@ -13,3 +13,40 @@ export function centerCamera(controls, particlesGroup) {
     // controls.enablePan = true;
   }
 }
+
+export function duplicateAnchorPoints() {
+  // TODO shift added point a bit + insert at correct position
+  const transform_object = this.transform_controls.object;
+  if (transform_object.name === "AnchorPoint" && !this._drawing) {
+    const index =
+      this.line3D.anchorPoints.children.indexOf(transform_object);
+
+    let point
+    if (index > 0) {
+      // Add the point between the current and the previous point
+      const obj_before = this.line3D.anchorPoints.children[index - 1];
+      const new_pos = obj_before.position
+        .clone()
+        .sub(transform_object.position)
+        .multiplyScalar(0.5)
+        .add(transform_object.position);
+
+      point = this.line3D.addPoint(new_pos, index);
+    } else {
+      if (this.line3D.anchorPoints.children.length > 1) {
+        // Add the point between the current and the next point
+        const obj_after = this.line3D.anchorPoints.children[index + 1];
+        const new_pos = obj_after.position
+          .clone()
+          .sub(transform_object.position)
+          .multiplyScalar(0.5)
+          .add(transform_object.position);
+
+        point = this.line3D.addPoint(new_pos, index - 1);
+      }
+
+    }
+    this.transform_controls.detach();
+    this.transform_controls.attach(point);
+  }
+}
--- zndraw/static/World/systems/select.js
+++ zndraw/static/World/systems/select.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { TransformControls } from "three/examples/jsm/controls/TransformControls.js";
-import { centerCamera } from "./events.js";
+import { centerCamera, duplicateAnchorPoints } from "./events.js";
 
 let scroll_timer = null;
 
@@ -244,43 +244,7 @@ class Selection {
 
         // make a copy of the currently selected point when d is pressed
         if (event.key === "d") {
-          // TODO shift added point a bit + insert at correct position
-          const transform_object = this.transform_controls.object;
-          if (transform_object.name === "AnchorPoint" && !this._drawing) {
-            const index =
-              this.line3D.anchorPoints.children.indexOf(transform_object);
-
-            let new_pos;
-            if (index > 0) {
-              // Add the point between the current and the previous point
-              const obj_before = this.line3D.anchorPoints.children[index - 1];
-              new_pos = obj_before.position
-                .clone()
-                .sub(transform_object.position)
-                .multiplyScalar(0.5)
-                .add(transform_object.position);
-              
-            const point = this.line3D.addPoint(new_pos, index);
-            this.transform_controls.detach();
-            this.transform_controls.attach(point);
-            } else {
-              if (this.line3D.anchorPoints.children.length > 1) {
-                // Add the point between the current and the next point
-                const obj_after = this.line3D.anchorPoints.children[index + 1];
-                new_pos = obj_after.position
-                  .clone()
-                  .sub(transform_object.position)
-                  .multiplyScalar(0.5)
-                  .add(transform_object.position);
-
-            const point = this.line3D.addPoint(new_pos, index - 1);
-            this.transform_controls.detach();
-            this.transform_controls.attach(point);
-              }
-
-            }
-
-          }
+          duplicateAnchorPoints.bind(this)();
         }
 
         if (event.key === "Backspace") {
