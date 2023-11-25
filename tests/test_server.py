e31335b42596920b04a00bd15d022e65b3e0b0d4
PythonFZ <fabian.zills@web.de>
2023-11-25 14:38:17 +0100

    improve testing

--- tests/test_server.py
+++ tests/test_server.py
@@ -14,11 +14,16 @@ class TestZnDraw:
         self.driver.get(server)
         assert self.driver.title == "ZnDraw"
 
-    def test_vis(self, server):
+    def test_vis_connection(self, server):
         self.driver.get(server)
         vis = ZnDraw(url=server)
         assert vis.socket.connected
-        for idx in range(10):
-            vis[idx] = molecule("H2O")
+    
+    def test_vis_len(self, server):
+        self.driver.get(server)
+        time.sleep(1) 
+        # we need to wait for all the data to be loaded.
+        # This includes jsonschemas and atoms.
+        vis = ZnDraw(url=server)
+        assert len(vis) == 1
 
-        assert len(vis) == 10
