a8bd8053e17ed920d6643c2df5a03f9219570f49
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-02-01 15:12:56 +0100

    longer timeout

--- zndraw/zndraw.py
+++ zndraw/zndraw.py
@@ -102,7 +102,7 @@ class ZnDrawBase:  # collections.abc.MutableSequence
         log.critical("Reconnecting to server")
         self.socket.disconnect()
         self._connect()
-        self.socket.sleep(1)
+        self.socket.sleep(5)
 
     def __len__(self) -> int:
         return int(self.socket.call("atoms:length", timeout=self.config.call_timeout))
