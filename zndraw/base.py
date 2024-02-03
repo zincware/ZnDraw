0888ccd4d1ce652ee95f0d45adb551b16ee1ccd7
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-02-03 12:42:19 +0100

    add base class for all pyclients

---
+++ zndraw/base.py
@@ -0,0 +1,65 @@
+import dataclasses
+from abc import abstractmethod
+import collections
+import socketio
+import typing
+import numpy as np
+
+@dataclasses.dataclass
+class ZnDrawBase(collections.abc.MutableSequence):
+    socket: socketio.Client
+    token: str
+
+    @abstractmethod
+    def log(self, message: str):
+        pass
+
+    @property
+    @abstractmethod
+    def bookmarks(self) -> dict[int, str]:
+        pass
+
+    @bookmarks.setter
+    @abstractmethod
+    def bookmarks(self, value: dict[int, str]):
+        pass
+
+    @property
+    @abstractmethod
+    def step(self) -> int:
+        pass
+
+    @step.setter
+    @abstractmethod
+    def step(self, value: int):
+        pass
+
+    @property
+    @abstractmethod
+    def selection(self) -> list[int]:
+        pass
+
+    @selection.setter
+    @abstractmethod
+    def selection(self, value: list[int]):
+        pass
+
+    @property
+    @abstractmethod
+    def points(self) -> np.ndarray:
+        pass
+
+    @points.setter
+    @abstractmethod
+    def points(self, value: np.ndarray):
+        pass
+
+    @property
+    @abstractmethod
+    def segments(self) -> np.ndarray:
+        pass
+
+    
+
+
+    
