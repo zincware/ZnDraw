372bc7d50c9cbdc6f8eb157a5da19c501b3b1df0
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-06-10 10:02:47 +0200

    remove some legacy code and unused packages

--- zndraw/utils.py
+++ zndraw/utils.py
@@ -1,4 +1,3 @@
-import contextlib
 import importlib.util
 import json
 import logging
@@ -7,42 +6,8 @@ import socket
 import sys
 import tempfile
 import uuid
-from typing import get_type_hints
 
-import ase
 import datamodel_code_generator
-import numpy as np
-import znframe
-from decorator import decorator
-
-SHARED = {"atoms": None}
-
-
-def split_list_into_chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i : i + n]
-
-
-def estimate_max_batch_size_for_socket(frames: list[znframe.Frame]):
-    from .settings import GlobalConfig
-
-    max_size = GlobalConfig().max_socket_data_size
-    sizes = [
-        sys.getsizeof(json.dumps(frame.to_dict(built_in_types=False))) for frame in frames
-    ]
-    largest_frame = max(sizes)
-    chunk_size = int(max_size / largest_frame * 0.9)
-    return max(chunk_size, 1)
-
-
-def rgb2hex(value):
-    r, g, b = np.array(value * 255, dtype=int)
-    return "#%02x%02x%02x" % (r, g, b)
-
-
-def get_radius(value):
-    return (0.25 * (2 - np.exp(-0.2 * value)),)
 
 
 def get_port(default: int = 1234) -> int:
@@ -58,15 +23,6 @@ def get_port(default: int = 1234) -> int:
     finally:
         sock.close()
     return port
-
-
-@contextlib.contextmanager
-def set_global_atoms(atoms: ase.Atoms):
-    """Temporarily create a 'SHARED["atoms"]' variable."""
-    # TODO: I know this is bad, but I don't know how to do it better - send help!
-    SHARED["atoms"] = atoms
-    yield
-    SHARED["atoms"] = None
 
 
 class ZnDrawLoggingHandler(logging.Handler):
@@ -116,50 +72,6 @@ def get_cls_from_json_schema(schema: dict, name: str, **kwargs):
         spec.loader.exec_module(module)
 
         return getattr(module, name)
-
-
-def hide_discriminator_field(d: dict) -> None:
-    """Inplace method to set discriminator field to hidden."""
-    raise ValueError("This method is not used anymore!")
-    for v in d.values():
-        if isinstance(v, dict):
-            if v.get("title") == "Discriminator":
-                v["options"] = {"hidden": True}
-                v["type"] = "string"
-            hide_discriminator_field(v)
-
-
-@decorator
-def typecast(func, *args, **kwargs):
-    annotations = get_type_hints(func)
-    updated_args = []
-    updated_kwargs = {}
-    for arg, arg_type in zip(args, annotations.values()):
-        if not isinstance(arg, arg_type):
-            if isinstance(arg, dict):
-                updated_args.append(arg_type(**arg))
-            else:
-                updated_args.append(arg_type(arg))
-        else:
-            updated_args.append(arg)
-
-    for kwarg, kwarg_type in annotations.items():
-        if kwarg in kwargs:
-            if not isinstance(kwargs[kwarg], kwarg_type):
-                if isinstance(kwargs[kwarg], dict):
-                    updated_kwargs[kwarg] = kwarg_type(**kwargs[kwarg])
-                else:
-                    updated_kwargs[kwarg] = kwarg_type(kwargs[kwarg])
-            else:
-                updated_kwargs[kwarg] = kwargs[kwarg]
-    return func(*updated_args, **updated_kwargs)
-
-
-@decorator
-def typecast_kwargs(func, **kwargs):
-    annotations = get_type_hints(func)
-    arg_type = list(annotations.values())[0]
-    return arg_type(**kwargs)
 
 
 def ensure_path(path: str):
