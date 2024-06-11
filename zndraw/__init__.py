b391f00ad4892dce32fa8e7873133ca152679a43
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-06-11 21:22:34 +0200

    remove eventlet monkey_patch

--- zndraw/__init__.py
+++ zndraw/__init__.py
@@ -1,14 +1,3 @@
-import os
-
-if "ZNDRAW_ENABLE_EVENTLET" in os.environ:
-    print("ZnDraw running with eventlet `monkey_patch` enabled.")
-
-    import eventlet
-
-    eventlet.monkey_patch()
-    # remove "ZNDRAW_ENABLE_EVENTLET" from the environment
-    os.environ.pop("ZNDRAW_ENABLE_EVENTLET")
-
 import importlib.metadata
 
 from zndraw.base import Extension
