edeeabb8a0c6e635fc45e60d03e18c1c78442b23
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-06-13 18:15:12 +0200

    test register modifier

--- tests/test_modifier.py
+++ tests/test_modifier.py
@@ -1,6 +1,6 @@
 import pytest
 
-from zndraw import ZnDraw
+from zndraw import Extension, ZnDraw
 
 
 def test_selection(server, s22):
@@ -23,7 +23,6 @@ def test_selection(server, s22):
     assert vis.selection == [0, 7, 6, 5, 4]
 
 
-# TODO: worker is probably not running
 def test_run_selection(server, s22):
     """Test the server fixture."""
     vis = ZnDraw(url=server, token="test_token")
@@ -33,3 +32,20 @@ def test_run_selection(server, s22):
     vis.socket.emit("selection:run", {"method": {"discriminator": "ConnectedParticles"}})
     vis.socket.sleep(5)
     assert vis.selection == [0, 1, 2, 3]
+
+
+def test_register_modifier(server, s22, water):
+    vis = ZnDraw(url=server, token="test_token")
+    vis.extend(s22)
+
+    class AppendWater(Extension):
+        def run(self, vis, **kwargs) -> None:
+            vis.append(water)
+            vis.step = len(vis) - 1
+
+    vis.register_modifier(AppendWater)
+
+    vis.socket.emit("modifier:run", {"method": {"discriminator": "AppendWater"}})
+    vis.socket.sleep(5)
+
+    assert vis.atoms == water
