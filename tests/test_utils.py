fd9722ac41140b76f5a316551d6f6c1b362075cb
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2024-08-08 12:16:21 +0200

    custom certificates and subdirectory hosted instances (#597)
    * add socket config
    
    * parse url
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * bugfix
    
    * append socket.io path
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * change default
    
    * use http_session to pass verify
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,20 @@
 import numpy as np
 import numpy.testing as npt
 
-from zndraw.utils import convert_url_to_http, direction_to_euler, euler_to_direction
+from zndraw.utils import (
+    convert_url_to_http,
+    direction_to_euler,
+    euler_to_direction,
+    parse_url,
+)
+
+
+def test_parse_url():
+    assert parse_url("http://example.com") == ("http://example.com", None)
+    assert parse_url("http://example.com/path") == ("http://example.com", "path")
+    assert parse_url("http://localhost:5000") == ("http://localhost:5000", None)
+    assert parse_url("http://localhost:5000/path") == ("http://localhost:5000", "path")
+    assert parse_url("http://localhost:5000/path/") == ("http://localhost:5000", "path")
 
 
 def test_conversion_utils():
