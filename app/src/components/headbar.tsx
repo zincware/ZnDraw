24e8dc410d52b04a1ccb2d26f3bf10c0e7c02e62
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-11 10:19:40 +0100

    update docs

--- app/src/components/headbar.tsx
+++ app/src/components/headbar.tsx
@@ -375,7 +375,7 @@ function HelpModel(props: any) {
 - **remove line point**: \`shift + backspace\`
 - **reset camera to original position**: \`keypress O\`
 - **rotate camera perpendicular to the screen**: \`keypress R\` or \`keypress ctrl + R\`
-- **Enter molecule editor (none, translate, rotate)**: \`keypress E\`
+- (experimental) **Enter molecule editor (none, translate, rotate)**: \`keypress E\`
 `;
 
   return (
