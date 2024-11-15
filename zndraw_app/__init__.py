0cc6919644003c4944e7e31d372257d31910438d
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-15 10:33:01 +0100

    make `ENGINEIO_PAYLOAD_MAX_DECODE_PACKETS` configurable via env

--- zndraw_app/__init__.py
+++ zndraw_app/__init__.py
@@ -1,3 +1,8 @@
 import eventlet
 
 eventlet.monkey_patch()
+
+import os
+from engineio.payload import Payload
+
+Payload.max_decode_packets = os.environ.get('ENGINEIO_PAYLOAD_MAX_DECODE_PACKETS', 16)
