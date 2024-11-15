7e79d8a89bf59d7231292dfba39aa5fd7bcc8ddf
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-15 10:34:26 +0100

    use walrus

--- zndraw_app/__init__.py
+++ zndraw_app/__init__.py
@@ -3,6 +3,8 @@ import eventlet
 eventlet.monkey_patch()
 
 import os
+
 from engineio.payload import Payload
 
-Payload.max_decode_packets = os.environ.get('ENGINEIO_PAYLOAD_MAX_DECODE_PACKETS', 16)
+if max_decode_packets := os.environ.get("ENGINEIO_PAYLOAD_MAX_DECODE_PACKETS"):
+    Payload.max_decode_packets = int(max_decode_packets)
