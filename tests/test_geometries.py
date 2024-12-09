c9c3036c2f428d4c2ac35cc7233ed27a1225847f
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-12-09 17:59:30 +0100

    test add geometries via selection

--- tests/test_geometries.py
+++ tests/test_geometries.py
@@ -1,7 +1,8 @@
 import pytest
 
 from zndraw import Box, Sphere, ZnDraw
-
+from ase.build import molecule
+import znsocket
 
 def test_geometries(server, s22):
     """Test the server fixture."""
@@ -24,3 +25,40 @@ def test_geometries(server, s22):
 
     with pytest.raises(ValueError):
         vis.geometries = "Geometries are not string!"
+
+
+def test_geometry_selection_position(server):
+    vis = ZnDraw(url=server, token="test_token")
+    vis.append(molecule("CH4"))
+    assert len(vis.geometries) == 0
+    geometry_queue = znsocket.Dict(
+        r=vis.r,
+        socket=vis._refresh_client,
+        key=f"queue:{vis.token}:geometry",
+    )
+    geometry_queue["Plane"] = {'material': {'color': '#62929e', 'opacity': 0.2, 'wireframe': False, 'outlines': False}, 'width': 10, 'height': 10}
+    vis.socket.emit("room:worker:run")
+    vis.socket.sleep(5)
+
+    assert len(vis.geometries) == 1
+    assert vis.geometries[0].position == [0, 0, 0]
+
+    # now with a selection
+    vis.selection = [1]
+    geometry_queue["Plane"] = {'material': {'color': '#62929e', 'opacity': 0.2, 'wireframe': False, 'outlines': False}, 'width': 10, 'height': 10}
+    vis.socket.emit("room:worker:run")
+    vis.socket.sleep(5)
+
+    assert len(vis.geometries) == 2
+    assert vis.geometries[1].position == vis.atoms.positions[1].tolist()
+    assert vis.geometries[1].position != [0, 0, 0]
+
+    # now with a selection of multiple atoms
+    vis.selection = [1, 2]
+    geometry_queue["Plane"] = {'material': {'color': '#62929e', 'opacity': 0.2, 'wireframe': False, 'outlines': False}, 'width': 10, 'height': 10}
+    vis.socket.emit("room:worker:run")
+    vis.socket.sleep(5)
+
+    assert len(vis.geometries) == 3
+    assert vis.geometries[2].position == vis.atoms.get_center_of_mass(indices=[1, 2]).tolist()
+    
