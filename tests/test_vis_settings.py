b512a8ea031122d342830825a9b042ad5b4e0154
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-10-03 23:16:54 +0200

    fix tests

--- tests/test_vis_settings.py
+++ tests/test_vis_settings.py
@@ -7,57 +7,57 @@ def test_vis_settings_same_room_same_user(server):
     client1 = ZnDraw(url=server, room="testroom", user="user1")
     client2 = ZnDraw(url=server, room="testroom", user="user1")
 
-    client1.settings.visualization.floor = True
+    client1.settings.scene.show_floor = True
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is True
-    assert client2.settings.visualization.floor is True
+    assert client1.settings.scene.show_floor is True
+    assert client2.settings.scene.show_floor is True
 
-    client1.settings.visualization.floor = False
+    client1.settings.scene.show_floor = False
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is False
-    assert client2.settings.visualization.floor is False
+    assert client1.settings.scene.show_floor is False
+    assert client2.settings.scene.show_floor is False
 
-    client2.settings.visualization.floor = True
+    client2.settings.scene.show_floor = True
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is True
-    assert client2.settings.visualization.floor is True
+    assert client1.settings.scene.show_floor is True
+    assert client2.settings.scene.show_floor is True
 
 
 def test_vis_settings_different_room_same_user(server):
     client1 = ZnDraw(url=server, room="room1", user="user1")
     client2 = ZnDraw(url=server, room="room2", user="user1")
 
-    client1.settings.visualization.floor = True
+    client1.settings.scene.show_floor = True
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is True
-    assert client2.settings.visualization.floor is False
+    assert client1.settings.scene.show_floor is True
+    assert client2.settings.scene.show_floor is False
 
-    client1.settings.visualization.floor = False
+    client1.settings.scene.show_floor = False
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is False
-    assert client2.settings.visualization.floor is False
+    assert client1.settings.scene.show_floor is False
+    assert client2.settings.scene.show_floor is False
 
-    client2.settings.visualization.floor = True
+    client2.settings.scene.show_floor = True
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is False
-    assert client2.settings.visualization.floor is True
+    assert client1.settings.scene.show_floor is False
+    assert client2.settings.scene.show_floor is True
 
 
 def test_vis_settings_same_room_different_user(server):
     client1 = ZnDraw(url=server, room="testroom", user="user1")
     client2 = ZnDraw(url=server, room="testroom", user="user2")
 
-    client1.settings.visualization.floor = True
+    client1.settings.scene.show_floor = True
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is True
-    assert client2.settings.visualization.floor is False
+    assert client1.settings.scene.show_floor is True
+    assert client2.settings.scene.show_floor is False
 
-    client1.settings.visualization.floor = False
+    client1.settings.scene.show_floor = False
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is False
-    assert client2.settings.visualization.floor is False
+    assert client1.settings.scene.show_floor is False
+    assert client2.settings.scene.show_floor is False
 
-    client2.settings.visualization.floor = True
+    client2.settings.scene.show_floor = True
     time.sleep(0.1)
-    assert client1.settings.visualization.floor is False
-    assert client2.settings.visualization.floor is True
+    assert client1.settings.scene.show_floor is False
+    assert client2.settings.scene.show_floor is True
