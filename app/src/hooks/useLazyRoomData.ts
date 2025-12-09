1eb8f036a807c98be085c755dffe594275a2adb2
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-12-09 14:41:02 +0100

    bugfix

--- app/src/hooks/useLazyRoomData.ts
+++ app/src/hooks/useLazyRoomData.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   getRoomInfo,
   getAllSelections,
